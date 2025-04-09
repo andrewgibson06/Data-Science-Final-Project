c3f18ef50aa9950fb1beb61b03f8dcb48a0e8db3
andrewgibson06 <andrewgibson06@yahoo.com>
2025-04-09 20:03:31 +0400

    Changed data cleaning to fit new nationalities

--- data_cleaning.py
+++ data_cleaning.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 #imports the csv files that were previously scraped
 batting_stats = pd.read_csv("ilt20_batting_stats_with_nationality_2025.csv")
-bowling_stats = pd.read_csv("ilt20_bowling_stats_2025.csv")
+bowling_stats = pd.read_csv("ilt20_bowling_stats_with_nationality_2025.csv")
 
 #does an outside join on the two data frames, as there may be players who did not bat or did not bowl
 merged_stats = pd.merge(batting_stats, bowling_stats, on="Player", how="outer", suffixes=("_batting", "_bowling"))
@@ -15,9 +15,18 @@ merged_stats.replace("-", 0, inplace=True)
 merged_stats["Team"] = merged_stats["Player"].str.extract(r"\((.*?)\)")
 merged_stats["Player"] = merged_stats["Player"].str.replace(r"\(.*?\)", "", regex=True)
 
+#merges the nationalities into one column
+merged_stats["Nationality"] = merged_stats["Nationality_batting"].combine_first(merged_stats["Nationality_bowling"])
+
+#removes the old nationality columns
+merged_stats = merged_stats.drop(columns=["Nationality_batting", "Nationality_bowling"])
+
+#does the same with the matches played
+merged_stats["Matches"] = merged_stats["Mat_batting"].combine_first(merged_stats["Mat_bowling"])
+merged_stats = merged_stats.drop(columns=["Mat_batting", "Mat_bowling"])
+
 #removes the span columns as they just say the season for whcih the data is taken from, as well as the player URL as thats not needed
-#the mat_bowling column is also removes as its the same as the batting, which will be renamed to just matches
-merged_stats = merged_stats.drop(columns=["Span_batting", "Span_bowling", "Player Profile URL", "Mat_bowling"])
+merged_stats = merged_stats.drop(columns=["Span_batting", "Span_bowling", "Player Profile URL_batting", "Player Profile URL_bowling"])
 merged_stats.rename(columns={"Mat_batting": "Matches"}, inplace=True)
 
 #saves the merged data frame to a new csv file
